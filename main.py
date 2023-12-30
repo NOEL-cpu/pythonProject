6a520df9e6fd756a4754732c22ccccd9d931fb7b
AdminX <gru70797@gmail.com>
2023-12-30 18:49:10 +0300

    Reloud

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
-#let's count the number of lines in a given file
+# let's count the number of lines in a given file
 
 import sys
 import os
 from pathlib import Path
 
-
-def resource_path(relative_path): #родственный путь что за он
+def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
-        print(base_path)
+        print("Показать путь который находится в файле спек", base_path)
     except Exception:
-        base_path = os.path.abspath(".")
-        print(base_path)
+        base_path = os.path.abspath(relative_path)
+        print("если тут значит создался по exception.  ", base_path)
 
     return os.path.join(base_path, relative_path)
 
 
+# ввод слова.
+# через tg бота?
+# a = str(input())
+a = 'утес'
 
-base_path = resource_path('folder')
-print(base_path)
-inp_path = Path(os.path.join(base_path, "inputs.txt"))
-print(base_path)
-file = open('text.txt', 'r')
-line_counter = 0
-word_counter = 0
+#2 Управление  анализатором лексемы для записи новых директорий с названиями
 
-for line in file:
-    words = line.split()
-    word_counter += len(words)
-    line_counter += 1
+for i in range(ord('а'), ord('я')):
+ #   print(chr(i), end='')
+    if a[0] == chr(i):
+        print(chr(i), 'Буква нашлась \n')
+        creatingfolder = 'C:/Users/AdminX/PycharmProjects/pythonProject/folder/'+a[0]
+        if  os.path.isdir(creatingfolder): #Если заглавная буква существует то создавать папку заглавной не надо
+            creatingfolder += '/' + a
+            if os.path.isdir(creatingfolder):# если такое слово уже есть то добавим к счетчику простмотров +1
+                print("!Такое слово уже есть")
+                creatingfolder += '/counter.txt'
+                file3=open(creatingfolder,'w')
+                file3.write('1raz')
+                file3.close()
+            else :# если такого слова нет( а только буква) то создадим
+                creatingfolder += '/' + a
+                os.mkdir(creatingfolder)
+        else :# если ни буквы ни директории не существует
+             os.mkdir(creatingfolder)
+             creatingfolder += '/' + a
+             os.mkdir(creatingfolder)
+# КОНЕЦ Управление  анализатором лексемы для записи новых директорий с названиями
 
-file.close()
-print('The number of line is:'+str(line_counter))
-print('The number of line is:'+str(word_counter))
-file2 = open('result.txt','w')
-file2.write('Agala ohyella')
-file2.close()
-a = int(input())
-b = int(input())
-s = a + b
-print(s)
-def main():
-    a=input("Enter a: ")
+print("Это в глабальной среде путь base_path=.", creatingfolder)
+#inp_path = Path(os.path.join(base_path, "inputs.txt"))
+
