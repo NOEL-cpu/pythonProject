3029f32caeee9b109a055fb0b77eb73daf6fe5d6
AdminX <gru70797@gmail.com>
2024-03-16 16:20:05 +0300

    работает запись в БД. Запускать надо не Main.

--- PutToDir.py
+++ PutToDir.py
@@ -24,12 +24,12 @@ def resource_path(relative_path):
 def alfabet(message):
     a = message  #не создает для заглавных букв!!!
     print(a)
-    for i in range(ord('а'), ord('я')):
+    for i in range(0x110000): # range(ord('а'), ord('я')):
         print(chr(i), end='')
         if a[0] == chr(i):# !!!!Нада сделать по всей длинне слова
             print(chr(i), 'Буква нашлась \n')
             creatingfolder = 'C:/Users/AdminX/PycharmProjects/pythonProject/folder/'+a[0]
-            print("Это в глабальной среде путь base_path=.", creatingfolder)
+            print("1Это в глабальной среде путь base_path=.", creatingfolder)
             if  os.path.isdir(creatingfolder): #Если заглавная буква существует то создавать папку заглавной не надо
                 creatingfolder += '/' + a
                 if os.path.isdir(creatingfolder):# если такое слово уже есть то добавим к счетчику простмотров +1
@@ -39,14 +39,14 @@ def alfabet(message):
                     file3.write('1raz')
                     file3.close()
                 else :# если такого слова нет( а только буква) то создадим
-                    creatingfolder += '/' + a
+
                     os.mkdir(creatingfolder)
-                    print("Это в глабальной среде путь base_path=.", creatingfolder)
+                    print("2Это в глабальной среде путь base_path=.", creatingfolder)
             else :# если ни буквы ни директории не существует
                  os.mkdir(creatingfolder)
                  creatingfolder += '/' + a
                  os.mkdir(creatingfolder)
-                 print("Это в глабальной среде путь base_path=.", creatingfolder)
+                 print("3Это в глабальной среде путь base_path=.", creatingfolder)
 
 # КОНЕЦ Управление  анализатором лексемы для записи новых директорий с названиями
 
