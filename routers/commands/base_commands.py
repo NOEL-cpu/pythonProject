8a62a88d1d96cb2f66602a21b4dce990b2189072
AdminX <gru70797@gmail.com>
2024-10-14 10:21:33 +0300

    done: connect like Syren's directores. keybords,media,routers, validators. Status: working with 70%percent bags. Gools function 20% Need will do:1!!!callbacc function for FlashCards. 1.1 progress in prepare callback for picture              2!!separate like users info. -> Create folder              3!usingindividual parameters for backup

--- routers/commands/base_commands.py
+++ routers/commands/base_commands.py
@@ -14,7 +14,11 @@ from keyboards.common_keyboards import (
     get_on_start_kb,
     get_on_help_kb,
     get_actions_kb,
+    inline_kb_for_traning1
 )
+from aiogram.fsm.context import FSMContext
+from aiogram.filters.state import State, StatesGroup
+
 from keyboards.inline_keyboards.info_kb import build_info_kb
 
 from googletrans import Translator
@@ -22,6 +26,7 @@ from googletrans import Translator
 import PutToDir
 
 from RebusCouch2 import RebusImageProcessor
+
 
 router = Router(name=__name__)
 
@@ -34,7 +39,7 @@ async def handle_start(message: types.Message):
     # Создаем клавиатуру с двумя кнопками
     keyboard = ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="/My progress"      ),
+            [KeyboardButton(text="/Тренировка"      ),
              KeyboardButton(text="/flashCardsStart"  ),
              KeyboardButton(text="/Memloud"          )]
 
@@ -54,8 +59,18 @@ async def handle_help(message: types.Message):
     text = "I'm an echo bot.\nSend me any message!"
     await message.answer(text=text)
 
+
+
+
+class GameState(StatesGroup):
+    game_in_process = State() # true/false
+    hidden_word = State ()      # like apple
+    counOfCoin = State()    # 1 +-0.1
+
+
+
 @router.message(Command("Тренировка"))
-async def handle_traning(message: types.Message):
+async def handle_start_game(message: types.Message,state: FSMContext):
     await message.answer(text="Тренировка началась")
     path = 'C:\\Users\\AdminX\\PycharmProjects\\pythonProject\\folder'
     result_image_path = "D:\\7 Photo\\DDs-bot\\4.png"
@@ -70,22 +85,69 @@ async def handle_traning(message: types.Message):
     photo1 = FSInputFile('D:\\7 Photo\\DDs-bot\\4.png')
     time.sleep(1)
     await message.answer(text="Я подумал")
+
+    #hw = str(Sesions[message.from_user.id].first_word)
+    #await message.answer(text=hw)
+
+    hw2 = rebus_processor.first_word.name
+    print(type(Sesions[message.from_user.id].first_word))
+
+    await state.update_data({"hidden_word": hw2})
+    await state.update_data({"counOfCoin": 1})#in FSM
+    await state.set_state(GameState.game_in_process)
+
+
+    await message.answer(hw2)
     await message.answer_photo(photo=photo1)
-    await message.answer(text="если вы не разгадали слово то вы можете воспользоваться подсказкой")
     await message.answer(text="ввести ответ:")
 
-@router.message(Command("подсказка"))
-async def resume_traning(message: types.message):
+    #
+
+
+
+
+
+
+
+@router.message(GameState.game_in_process)
+async def resume_traning(message: types.message, state: FSMContext):
+
+    rebus_processor2=Sesions[message.from_user.id] # получили  обьект класса из глобальной среды
+    hidden_word=rebus_processor2.first_word.name
+    text_user = message.text
+    if(hidden_word == text_user):
+        await message.answer("Winner!")
+        await state.clear()
+        return 0
+
+    else:
+        await message.answer("loser")
+
+
     await message.answer(text="Тренировка продолжается")
     path = 'C:\\Users\\AdminX\\PycharmProjects\\pythonProject\\folder'
     result_image_path = "D:\\7 Photo\\DDs-bot\\result.jpg"
     user_try = 0
-    rebus_processor2=Sesions[message.from_user.id] # получили  обьект класса из глобальной среды
-    
+
     rebus_processor2.process_hint(user_query="2")
     await message.answer(text="Я думаю")
     photo1 = FSInputFile('D:\\7 Photo\\DDs-bot\\4.png')
-    await message.answer_photo(photo=photo1)
+    #await message.answer_photo(photo=photo1)
+
+     # Создаем инлайн-клавиатуру с 5 кнопками
+    markup = inline_kb_for_traning1()
+    # Отправляем фото с текстом и инлайн-клавиатурой
+    sent_message = await message.answer_photo(
+        photo=photo1,
+        caption="Это ваша картинка и описание с выбором кнопок.",
+        reply_markup=markup,
+        reply_to_message_id=message.message_id  # Отправляем как ответ на сообщение пользователя
+    )
+
+    # Возвращаем ID отправленного сообщения
+    #sent_message_id = sent_message.message_id
+    #await message.answer(text=f"ID этого сообщения: {sent_message_id}")
+
 
     # серриализация(сохранение) информации о текущем обекте для последующей тренировки ...info.txt
 
@@ -98,6 +160,55 @@ async def resume_traning(message: types.message):
     #     photo2 = FSInputFile('D:\\7 Photo\\DDs-bot\\result.jpg')
     #     await message.answer_photo(photo=photo2)
 
+
+@router.message()
+async def echo_message(message: types.Message):
+    await message.answer(text="I am start")
+    try:
+
+        text11 = message.text
+        await message.answer(text11)
+        # Trenning
+        await message.answer(text="1")
+
+
+        # Блок перевода
+        translator = Translator()
+        translator_word = translator.translate(text11, src='en', dest='ru').text
+        await message.answer(translator_word)
+        # print(text11, translator_word)
+
+        # Раскладка слова в директорию и ответ есть ли картинка для слова
+        pathOfpictureForSendUser, InfoTxt = PutToDir.alfabet(text11)
+        print(pathOfpictureForSendUser)
+
+        if pathOfpictureForSendUser != 'NOTPicture':
+            photo22 = FSInputFile(pathOfpictureForSendUser)
+          #  photo22 = 'D:/7 Photo/DDs-bot/1.jpg'
+            await message.answer_photo( photo=photo22)
+
+        await message.reply(text="Количество запросов = " + str(InfoTxt))
+
+    except ValueError as exc:
+        print(exc.with_traceback())
+        print('Искл Value erorr1 (потому что не текст)')
+
+    except TypeError as exc:
+        print('Отработал exeption 70 str', exc.with_traceback())
+        await message.reply(text="Something new 🙂")
+
+    try:
+        # Подготовка имени картинки
+        base_url = "C://Users//AdminX//PycharmProjects//pythonProject//folder//bankOfPictures//"
+        length = 8  # Длина случайной строки
+        letters_and_digits = string.ascii_letters + string.digits
+        random_string = ''.join(random.choice(letters_and_digits) for i in range(length))
+        random_filename = random_string + ".jpg"
+        new_url = base_url + random_filename
+        print(new_url)
+
+    except ValueError as exc2:
+        print('Искл Value erorr2 (потому что не картинка)')
 @router.message(Command("Конец"))
 async def handle_traning(message: types.Message):
      await message.answer(text="я удаляю предыдущюю тренировку")
@@ -151,7 +262,7 @@ async def handle_training(message: types.Message):
     await message.answer(text="Я подумал")
 
     # Создаем инлайн-клавиатуру с 5 кнопками
-    markup = get_actions_kb()
+    markup = inline_kb_for_traning1()
     # Отправляем фото с текстом и инлайн-клавиатурой
     sent_message = await message.answer_photo(
         photo=photo,
@@ -166,55 +277,4 @@ async def handle_training(message: types.Message):
 
     # Этот ID можно сохранить для будущих ответов, например, в базе данных или глобальном словаре
     # Например: Sessions[message.from_user.id]["last_message_id"] = sent_message_id
-
-
-@router.message()
-async def echo_message(message: types.Message):
-    await message.answer(text="I am start")
-    try:
-
-        text11 = message.text
-        await message.answer(text11)
-        # Trenning
-        await message.answer(text="1")
-
-
-        # Блок перевода
-        translator = Translator()
-        translator_word = translator.translate(text11, src='en', dest='ru').text
-        await message.answer(translator_word)
-        # print(text11, translator_word)
-
-        # Раскладка слова в директорию и ответ есть ли картинка для слова
-        pathOfpictureForSendUser, InfoTxt = PutToDir.alfabet(text11)
-        print(pathOfpictureForSendUser)
-
-        if pathOfpictureForSendUser != 'NOTPicture':
-            photo22 = FSInputFile(pathOfpictureForSendUser)
-          #  photo22 = 'D:/7 Photo/DDs-bot/1.jpg'
-            await message.answer_photo( photo=photo22)
-
-        await message.reply(text="Количество запросов = " + str(InfoTxt))
-
-    except ValueError as exc:
-        print(exc.with_traceback())
-        print('Искл Value erorr1 (потому что не текст)')
-
-    except TypeError as exc:
-        print('Отработал exeption 70 str', exc.with_traceback())
-        await message.reply(text="Something new 🙂")
-
-    try:
-        # Подготовка имени картинки
-        base_url = "C://Users//AdminX//PycharmProjects//pythonProject//folder//bankOfPictures//"
-        length = 8  # Длина случайной строки
-        letters_and_digits = string.ascii_letters + string.digits
-        random_string = ''.join(random.choice(letters_and_digits) for i in range(length))
-        random_filename = random_string + ".jpg"
-        new_url = base_url + random_filename
-        print(new_url)
-
-    except ValueError as exc2:
-        print('Искл Value erorr2 (потому что не картинка)')
-
 
